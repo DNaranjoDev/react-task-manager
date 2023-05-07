a5cd0972f1a742e29c13b3e2fda2e454fd3747d8
DNaranjoDev <davidnaranjodev@gmail.com>
2023-05-07 19:45:54 +0100

    Add useHttp to the Get response

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,44 +3,31 @@ import { Inter } from 'next/font/google'
 import { Fragment, useEffect, useState } from 'react'
 import NewTask from '@/components/NewTask/NewTask'
 import Tasks from '@/components/Tasks/Tasks'
+import useHttp from '@/hooks/use-http'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState(null)
   const [tasks, setTasks] = useState([])
 
-  const fetchTasks = async (taskText) => {
-    setIsLoading(true)
-    setError(null)
-    try {
-      const response = await fetch(
-        'https://react-http-fb501-default-rtdb.europe-west1.firebasedatabase.app/task.json'
-      )
-
-      if (!response.ok) {
-        throw new Error('Request failed!')
-      }
-
-      const data = await response.json()
-
-      const loadedTasks = []
-
-      for (const taskKey in data) {
-        loadedTasks.push({ id: taskKey, text: data[taskKey].text })
-      }
-
-      setTasks(loadedTasks)
-    } catch (err) {
-      setError(err.message || 'Something went wrong!')
-    }
-    setIsLoading(false)
-  }
+  const { isLoading, error, sendRequest: fetchTasks } = useHttp()
 
   useEffect(() => {
-    fetchTasks()
-  }, [])
+    const transformTasks = (tasksObj) => {
+      const loadedTasks = []
+  
+      for (const taskKey in tasksObj) {
+        loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text })
+      }
+  
+      setTasks(loadedTasks)
+    }
+
+    fetchTasks(
+      {url: 'https://react-http-fb501-default-rtdb.europe-west1.firebasedatabase.app/task.json'},
+      transformTasks
+    )
+  }, [fetchTasks])
 
   const taskAddHandler = (task) => {
     setTasks((prevTasks) => prevTasks.concat(task));
